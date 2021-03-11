e3d06e410b98a827969720d93dcab17ef87b36e7
michinosuke <michiya.318.3738@gmail.com>
2021-03-11 23:54:22 +0900

    コメントを消した

--- main.py
+++ main.py
@@ -13,14 +13,10 @@ url = 'https://disboard.org/ja/dashboard/servers'
 
 def main():
     options = Options()
-    options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-    # options.add_argument('--headless')
+    # options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+    options.add_argument('--headless')
     options.add_argument('window-size=1600,900')
-    # options.add_argument('--user-data-dir=/Users/michinosuke/Library/Application Support/Google/Chrome Canary')
-    options.add_argument('--profile-directory=Default')
-
-    driver = webdriver.Chrome(options=options, executable_path='/Users/michinosuke/archive/others/webdriver/90/chromedriver')
-    # driver = webdriver.Chrome(options=options, executable_path='/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(options=options, executable_path='/usr/local/bin/chromedriver')
 
     try:
         driver.get(url)
@@ -36,11 +32,8 @@ def main():
         sleep(3)
 
         verify_button = wait.until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'lookFilled-1Gx00P')))
-        # verify_button = driver.find_elements_by_css_selector('button.lookFilled-1Gx00P')
         if verify_button:
             verify_button.click()
-        # verify_buttons = wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, 'button.lookFilled-1Gx00P')))
-        # verify_buttons.click()
         element = wait.until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'button-bump')))
         driver.find_element_by_class_name('button-bump').click()
         sleep(10)
